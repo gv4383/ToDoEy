1b58d5d086f33bbaa8b8dd7f5a5bfc33bc592b31
gv4383 <gregory.vargas18@gmail.com>
2022-01-17 22:27:48 -0600

    Initial Commit

---
+++ ToDoEy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoEy
+//
+//  Created by Goyo Vargas on 1/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoEy/ToDoEyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoEyApp.swift
+//  ToDoEy
+//
+//  Created by Goyo Vargas on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoEyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
